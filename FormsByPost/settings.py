b182d71e119ff0db53c0c6a5fb4c74acb917cae2
indiresham-harsha-vardhan <indreshamharshavardhan@gmail.com>
2025-01-06 20:53:37 +0530

    kk

--- FormsByPost/settings.py
+++ FormsByPost/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR=os.path.join(BASE_DIR,'template')
+TEMPLATE_DIR=os.path.join(BASE_DIR,'template')
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 # Quick-start development settings - unsuitable for production
@@ -44,8 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', 
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
